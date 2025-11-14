d3e36dc3f15d405cefd63b0d9735e20469829113
Luca Bernstein <luca.bernstein@sap.com>
2025-11-14 13:47:42 +0100

    Refactor: Extract file handling from meta/diff to kustomization/writer

--- pkg/utilities/components/meta/diff_test.go
+++ pkg/utilities/components/meta/diff_test.go
@@ -10,7 +10,6 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/spf13/afero"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
@@ -35,38 +34,7 @@ var (
 )
 
 var _ = Describe("Meta Dir Config Diff", func() {
-	var fs afero.Afero
-
-	BeforeEach(func() {
-		fs = afero.Afero{Fs: afero.NewMemMapFs()}
-	})
-
-	Describe("#CreateOrUpdateManifest", func() {
-		It("should overwrite the manifest file if no meta file is present yet", func() {
-			obj := &corev1.ConfigMap{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "v1",
-					Kind:       "ConfigMap",
-				},
-				Data: map[string]string{
-					"key": "value",
-				},
-			}
-
-			objYaml, err := yaml.Marshal(obj)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(meta.CreateOrUpdateManifest(objYaml, "/landscape", "manifest/config.yaml", fs)).To(Succeed())
-
-			content, err := fs.ReadFile("/landscape/.glk/defaults/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(content)).To(MatchYAML(expectedDefaultConfigMapOutput))
-
-			content, err = fs.ReadFile("/landscape/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(content)).To(MatchYAML(expectedDefaultConfigMapOutput))
-		})
-
+	Describe("#ThreeWayMergeManifest", func() {
 		It("should patch only changed default values on subsequent generates and retain custom modifications", func() {
 			obj := &corev1.ConfigMap{
 				TypeMeta: metav1.TypeMeta{
@@ -81,13 +49,11 @@ var _ = Describe("Meta Dir Config Diff", func() {
 			objYaml, err := yaml.Marshal(obj)
 			Expect(err).NotTo(HaveOccurred())
 
-			Expect(meta.CreateOrUpdateManifest(objYaml, "/landscape", "manifest/config.yaml", fs)).To(Succeed())
+			newContents, err := meta.ThreeWayMergeManifest(nil, objYaml, nil)
+			Expect(err).NotTo(HaveOccurred())
 
 			// Modify the manifest on disk
-			content, err := fs.ReadFile("/landscape/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			modifiedContent := []byte(strings.ReplaceAll(string(content), "value", "changedValue"))
-			Expect(fs.WriteFile("/landscape/manifest/config.yaml", modifiedContent, 0600)).To(Succeed())
+			content := []byte(strings.ReplaceAll(string(newContents), "value", "changedValue"))
 
 			// Patch the default object and generate again
 			obj = obj.DeepCopy()
@@ -96,47 +62,23 @@ var _ = Describe("Meta Dir Config Diff", func() {
 				"newKey": "anotherValue",
 			}
 
-			objYaml, err = yaml.Marshal(obj)
+			newObjYaml, err := yaml.Marshal(obj)
 			Expect(err).NotTo(HaveOccurred())
 
-			Expect(meta.CreateOrUpdateManifest(objYaml, "/landscape", "manifest/config.yaml", fs)).To(Succeed())
+			content, err = meta.ThreeWayMergeManifest(objYaml, newObjYaml, content)
+			Expect(err).NotTo(HaveOccurred())
 
-			content, err = fs.ReadFile("/landscape/.glk/defaults/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(content)).To(MatchYAML(expectedConfigMapOutputWithNewKey))
-
-			content, err = fs.ReadFile("/landscape/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
 			Expect(string(content)).To(MatchYAML(strings.ReplaceAll(expectedConfigMapOutputWithNewKey, "key: value", "key: changedValue")))
 		})
 
 		It("should support patching raw yaml manifests with comments", func() {
-			Expect(meta.CreateOrUpdateManifest([]byte(manifestDefault), "/landscape", "manifest/config.yaml", fs)).To(Succeed())
-
-			content, err := fs.ReadFile("/landscape/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(content)).To(Equal(manifestDefault))
-
-			// Modify the manifest on disk
-			Expect(fs.WriteFile("/landscape/manifest/config.yaml", []byte(manifestEdited), 0600)).To(Succeed())
-
-			// Run the generation again with an updated default manifest
-			Expect(meta.CreateOrUpdateManifest([]byte(manifestDefaultNew), "/landscape", "manifest/config.yaml", fs)).To(Succeed())
-
-			content, err = fs.ReadFile("/landscape/manifest/config.yaml")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(string(content)).To(Equal(manifestGenerated))
+			mergedManifest, err := meta.ThreeWayMergeManifest([]byte(manifestDefault), []byte(manifestDefaultNew), []byte(manifestEdited))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(mergedManifest)).To(Equal(manifestGenerated))
 		})
 
 		It("should handle a non-existent default file gracefully", func() {
-			// User manifest is there first, no default file yet
-			Expect(fs.WriteFile("/landscape/manifest/config.yaml", []byte(strings.ReplaceAll(expectedDefaultConfigMapOutput, "key: value", "key: newDefaultValue")), 0600)).To(Succeed())
-
-			// Run the generation
-			Expect(meta.CreateOrUpdateManifest([]byte(expectedConfigMapOutputWithNewKey), "/landscape", "manifest/config.yaml", fs)).To(Succeed())
-
-			// The previous user manifest content should have been retained, only new keys added
-			content, err := fs.ReadFile("/landscape/manifest/config.yaml")
+			content, err := meta.ThreeWayMergeManifest(nil, []byte(expectedConfigMapOutputWithNewKey), []byte(strings.ReplaceAll(expectedDefaultConfigMapOutput, "key: value", "key: newDefaultValue")))
 			Expect(err).ToNot(HaveOccurred())
 			Expect(string(content)).To(Equal(strings.ReplaceAll(expectedConfigMapOutputWithNewKey, "key: value", "key: newDefaultValue") + "\n"))
 		})
