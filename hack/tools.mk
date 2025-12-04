7b20f5427dee7685219df43e4427b5f384e221a4
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-12-04 14:44:59 +0100

    Update dependency fluxcd/flux2 to v2.7.5 (#22)
    * Update dependency fluxcd/flux2 to v2.7.5
    
    * ensure version file for flux
    
    * make generate
    
    ---------
    
    Co-authored-by: Martin Weindel <martin.weindel@sap.com>

--- hack/tools.mk
+++ hack/tools.mk
@@ -3,12 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # renovate: datasource=github-releases depName=fluxcd/flux2
-FLUX_CLI_VERSION ?= v2.7.3
+FLUX_CLI_VERSION ?= v2.7.5
 
 FLUX_CLI ?= $(TOOLS_DIR)/bin/$(SYSTEM_NAME)-$(SYSTEM_ARCH)/flux
 .PHONY: flux-cli
 flux-cli: $(FLUX_CLI)
-$(FLUX_CLI): $(TOOLS_DIR)
+$(FLUX_CLI): $(TOOLS_DIR) $(call tool_version_file,$(FLUX_CLI),$(FLUX_CLI_VERSION))
 	curl -Lo $(FLUX_CLI).tar.gz https://github.com/fluxcd/flux2/releases/download/$(FLUX_CLI_VERSION)/flux_$(FLUX_CLI_VERSION:v%=%)_$(SYSTEM_NAME)_$(SYSTEM_ARCH).tar.gz
 	tar -zxvf $(FLUX_CLI).tar.gz -C $(TOOLS_DIR)/bin/$(SYSTEM_NAME)-$(SYSTEM_ARCH) flux
 	touch $(FLUX_CLI) && chmod +x $(FLUX_CLI) && rm $(FLUX_CLI).tar.gz
