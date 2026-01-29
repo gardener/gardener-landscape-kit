b8f498a38db6dcfe2c865cb9000a24a41e2009b2
Luca Bernstein <luca.bernstein@sap.com>
2026-01-29 14:20:21 +0100

    Add `provider-aws` extension component (#79)
    * Add provider-aws extension component
    
    * Fix casing

--- pkg/registry/register.go
+++ pkg/registry/register.go
@@ -14,8 +14,9 @@ import (
 	"github.com/gardener/gardener-landscape-kit/pkg/apis/config/v1alpha1"
 	"github.com/gardener/gardener-landscape-kit/pkg/components"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/flux"
-	networking_calico "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-calico"
-	networking_cilium "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-cilium"
+	networkingCalico "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-calico"
+	networkingCilium "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/networking-cilium"
+	providerAWS "github.com/gardener/gardener-landscape-kit/pkg/components/gardener-extensions/provider-aws"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/gardener/garden"
 	"github.com/gardener/gardener-landscape-kit/pkg/components/gardener/operator"
 )
@@ -25,8 +26,9 @@ var ComponentList = []func() components.Interface{
 	flux.NewComponent,
 	operator.NewComponent,
 	garden.NewComponent,
-	networking_calico.NewComponent,
-	networking_cilium.NewComponent,
+	networkingCalico.NewComponent,
+	networkingCilium.NewComponent,
+	providerAWS.NewComponent,
 }
 
 // RegisterAllComponents registers all available components.
