062786e086e648570eb780dd746305bc2ff72e80
Luca Bernstein <luca.bernstein@sap.com>
2026-02-04 10:23:37 +0100

    Add basic e2e test setup (#75)
    * Add basic e2e test setup
    
    * Address review comments: Add custom host names
    
    * Add added components (Operator extensions) to wait for and ensure installed
    
    * Increase timeout for Garden reconciliation
    
    * Ensure `kind` network is present
    
    * Add default git user with email to test base and landscape repo

---
+++ hack/ci-e2e-kind.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+source $GARDENER_HACK_DIR/ci-common.sh
+source $(dirname $0)/ci-common.sh
+
+clamp_mss_to_pmtu
+
+glk_ensure_local_gardener_cloud_hosts
+
+# test setup
+make kind-up e2e-prepare
+
+# export all container logs and events after test execution
+trap "
+  ( export_artifacts "glk-single" )
+  ( make kind-down )
+" EXIT
+
+make test-e2e-local
