141925c90e399aa14c4c62e968393d4e82c61ef5
Tim Usner <tim.usner@sap.com>
2026-02-16 09:23:18 +0100

    Address review comment

--- hack/tools.mk
+++ hack/tools.mk
@@ -2,12 +2,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-GLK_GLK             := $(TOOLS_BIN_DIR)/gardener-landscape-kit
 GLK_PRETTIFY        := $(TOOLS_BIN_DIR)/prettify
 
 # renovate: datasource=github-releases depName=fluxcd/flux2
 FLUX_CLI_VERSION ?= v2.7.5
-GLK_GLK_VERSION = $(shell git rev-parse HEAD)
 GLK_PRETTIFY_VERSION = $(shell git rev-parse HEAD)
 
 FLUX_CLI ?= $(TOOLS_DIR)/bin/$(SYSTEM_NAME)-$(SYSTEM_ARCH)/flux
