86be7133d814fcc8d3b7566586f5dc6d910b7293
Luca Bernstein <luca.bernstein@sap.com>
2026-02-17 14:54:04 +0100

    Add `virtual-garden-access-flux` component
    So Flux can access the virtual garden.

---
+++ pkg/components/gardener/virtual-garden-access/component.go
@@ -0,0 +1,94 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package virtualgardenaccess
+
+import (
+	"embed"
+	"path"
+
+	"github.com/gardener/gardener-landscape-kit/pkg/components"
+	"github.com/gardener/gardener-landscape-kit/pkg/utils/files"
+)
+
+const (
+	// ComponentName is the name of the virtual-garden-access component.
+	ComponentName = "virtual-garden-access"
+	// ComponentDirectory is the directory of the virtual-garden-access component within the base components directory.
+	ComponentDirectory = "gardener/virtual-garden-access"
+)
+
+var (
+	// baseTemplateDir is the directory where the base templates are stored.
+	baseTemplateDir = "templates/base"
+	//go:embed templates/base
+	baseTemplates embed.FS
+
+	// landscapeTemplateDir is the directory where the landscape templates are stored.
+	landscapeTemplateDir = "templates/landscape"
+	//go:embed templates/landscape
+	landscapeTemplates embed.FS
+)
+
+type component struct{}
+
+// NewComponent creates a new virtual-garden-access component.
+func NewComponent() components.Interface {
+	return &component{}
+}
+
+// Name returns the component name.
+func (c *component) Name() string {
+	return ComponentName
+}
+
+// GenerateBase generates the component base directory.
+func (c *component) GenerateBase(options components.Options) error {
+	for _, op := range []func(components.Options) error{
+		writeBaseTemplateFiles,
+	} {
+		if err := op(options); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// GenerateLandscape generates the component landscape directory.
+func (c *component) GenerateLandscape(options components.LandscapeOptions) error {
+	for _, op := range []func(components.LandscapeOptions) error{
+		writeLandscapeTemplateFiles,
+	} {
+		if err := op(options); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeBaseTemplateFiles(opts components.Options) error {
+	objects, err := files.RenderTemplateFiles(baseTemplates, baseTemplateDir, nil)
+	if err != nil {
+		return err
+	}
+
+	return files.WriteObjectsToFilesystem(objects, opts.GetTargetPath(), path.Join(components.DirName, ComponentDirectory), opts.GetFilesystem())
+}
+
+func writeLandscapeTemplateFiles(opts components.LandscapeOptions) error {
+	var (
+		relativeComponentPath = path.Join(components.DirName, ComponentDirectory)
+		relativeRepoRoot      = files.CalculatePathToComponentBase(opts.GetRelativeLandscapePath(), relativeComponentPath)
+	)
+
+	objects, err := files.RenderTemplateFiles(landscapeTemplates, landscapeTemplateDir, map[string]any{
+		"relativePathToBaseComponent": path.Join(relativeRepoRoot, opts.GetRelativeBasePath(), relativeComponentPath),
+		"landscapeComponentPath":      path.Join(opts.GetRelativeLandscapePath(), relativeComponentPath),
+	})
+	if err != nil {
+		return err
+	}
+
+	return files.WriteObjectsToFilesystem(objects, opts.GetTargetPath(), path.Join(components.DirName, ComponentDirectory), opts.GetFilesystem())
+}
