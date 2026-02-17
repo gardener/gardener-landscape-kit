c62c6cd1a2e2b2959fae2c40b32cf99e60edc58c
Luca Bernstein <luca.bernstein@sap.com>
2026-02-17 16:06:36 +0100

    Add `virtual-garden/garden-project` component
    This makes use of the flux kubeconfig created before.

---
+++ pkg/components/virtual-garden/garden-project/component_test.go
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gardenproject_test
+
+import (
+	"github.com/go-logr/logr"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/afero"
+
+	"github.com/gardener/gardener-landscape-kit/pkg/apis/config/v1alpha1"
+	"github.com/gardener/gardener-landscape-kit/pkg/cmd"
+	generateoptions "github.com/gardener/gardener-landscape-kit/pkg/cmd/generate/options"
+	"github.com/gardener/gardener-landscape-kit/pkg/components"
+	gardenproject "github.com/gardener/gardener-landscape-kit/pkg/components/virtual-garden/garden-project"
+)
+
+var _ = Describe("Component Generation", func() {
+	var (
+		fs           afero.Afero
+		cmdOpts      *cmd.Options
+		generateOpts *generateoptions.Options
+	)
+
+	BeforeEach(func() {
+		fs = afero.Afero{Fs: afero.NewMemMapFs()}
+		cmdOpts = &cmd.Options{Log: logr.Discard()}
+		generateOpts = &generateoptions.Options{
+			TargetDirPath: "/repo/baseDir",
+			Options:       cmdOpts,
+		}
+	})
+
+	Describe("#GenerateBase", func() {
+		var opts components.Options
+
+		BeforeEach(func() {
+			var err error
+			opts, err = components.NewOptions(generateOpts, fs)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should generate the component base", func() {
+			component := gardenproject.NewComponent()
+			Expect(component.GenerateBase(opts)).To(Succeed())
+
+			for _, file := range []string{
+				"/repo/baseDir/.glk/defaults/components/virtual-garden/garden-project/project.yaml",
+				"/repo/baseDir/components/virtual-garden/garden-project/project.yaml",
+			} {
+				content, err := fs.ReadFile(file)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(content)).To(ContainSubstring("Project"))
+			}
+
+			content, err := fs.ReadFile("/repo/baseDir/components/virtual-garden/garden-project/kustomization.yaml")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(content)).To(ContainSubstring("- project.yaml"))
+		})
+	})
+
+	Describe("#GenerateLandscape", func() {
+		BeforeEach(func() {
+			generateOpts.TargetDirPath = "/repo/landscapeDir"
+			generateOpts.Config = &v1alpha1.LandscapeKitConfiguration{
+				Git: &v1alpha1.GitRepository{Paths: v1alpha1.PathConfiguration{Landscape: "./landscapeDir", Base: "./baseDir"}},
+			}
+		})
+
+		It("should generate only the flux kustomization into the landscape dir", func() {
+			component := gardenproject.NewComponent()
+			landscapeOpts, err := components.NewLandscapeOptions(generateOpts, fs)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(component.GenerateLandscape(landscapeOpts)).To(Succeed())
+
+			exists, err := fs.DirExists("/repo/baseDir")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(exists).To(BeFalse())
+
+			content, err := fs.ReadFile("/repo/landscapeDir/components/virtual-garden/garden-project/flux-kustomization.yaml")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(content)).To(ContainSubstring("path: landscapeDir/components/virtual-garden/garden-project"))
+
+			content, err = fs.ReadFile("/repo/landscapeDir/components/virtual-garden/garden-project/kustomization.yaml")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(content)).To(ContainSubstring("- ../../../../baseDir/components/virtual-garden/garden-project"))
+		})
+	})
+})
