b747a7ab86b6df3540f77b55ff66e3eeec45ab77
Tim Usner <tim.usner@sap.com>
2026-04-27 07:48:21 +0200

    Add Forgejo Runner

--- dev-setup/git-server/entrypoint.sh
+++ dev-setup/git-server/entrypoint.sh
@@ -27,12 +27,29 @@ create_user() {
     --password testtest \
     --email gitops@local.gardener.cloud \
     --admin"
+}
 
-  touch /data/.glk-initialisation-finished
+generate_runner_token() {
+  # Generate runner token
+  TOKEN=$(su - git -c "/usr/local/bin/forgejo forgejo-cli actions generate-secret")
+
+  # Register the runner token
+  su - git -c "/usr/local/bin/forgejo forgejo-cli actions register --secret $TOKEN --keep-labels"
+
+  # Generate UUID from token (first 16 bytes)
+  UUID=$(echo -n "$TOKEN" | head -c 16 | xxd -p -c 16 | sed -E 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')
+
+  # Create runner directory and copy config
+  mkdir -p /data/runner
+  cp /runner-config.yaml /data/runner/config.yaml
+
+  # Replace placeholders in config
+  sed -i "s|<TOKEN>|$TOKEN|g" /data/runner/config.yaml
+  sed -i "s|<UUID>|$UUID|g" /data/runner/config.yaml
 }
 
 if [ ! -f /data/.glk-initialisation-finished ]; then
-  create_user &
+  create_user && generate_runner_token && touch /data/.glk-initialisation-finished &
 else
   echo "glk initialisation skipped"
 fi
